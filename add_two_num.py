5be5f1a8d24acf551e3e9bcf57cd6ae8386466fe
Jr Joy <joy2244680@gmail.com>
2025-08-06 23:39:30 +0600

    new files

---
+++ add_two_num.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the first value :"))
+b = int(input("Enter the second value :"))
+
+sum = a + b
+
+print("The result of add two numbers is :",sum)
---
+++ array_1.py
@@ -0,0 +1,28 @@
+'''
+# Using For Loop
+j = []
+
+o = int(input("Enter the number of element : "))
+
+for i in range(o):
+    element = int(input(f"Enter the number {i+1} : "))
+    j.append(element)
+
+print("The Array is = ",j)
+
+'''
+
+
+# Using while loop 
+j = []
+
+o = int(input("Enter the num of elements :"))
+
+i = 0
+
+while(i < o):
+    y = int(input(f"Enter the num {i+1} :"))
+    j.append(y)
+    i += 1
+
+print("The Array is : ",j)
