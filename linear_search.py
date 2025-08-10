358a090b6242d50121dc7845fd98040a0a8cd7c3
Jr Joy <joy2244680@gmail.com>
2025-08-10 18:08:03 +0600

    updated

--- linear_search.py
+++ linear_search.py
@@ -3,23 +3,19 @@
 #then output showing by condition.....
 
 given_list = [10, 20, 30, 40, 50, 700]
+target = int(input("Enter the number : "))
 
-def linear_search(arr, target):
+def linear_search(given_list,target): #create a function with list & targrt
+    for index in range(len(given_list)): #Getting the index of list
+        if(given_list[index]==target): #Checking the list element through index with target element
+            return index  #If its equal then return the index number
+    return -1  #Neither -1
 
-    for i in range(len(arr)):
-        if arr[i] == target:
-            return i 
-    return -1 
- 
-while True:
-    find_value = input("Enter a number to search (or '0' to exit): ")
+result = linear_search(given_list, target) #Storing the function result in a variable
 
-    if find_value == '0':
-        break
+#Condition for printing output
+if(result != -1):  
+    print(f"The element is fount at index : {result}")
+else:
+    print("The element not found.")
 
-    result = linear_search(given_list, int(find_value))
-
-    if result != -1:
-        print("the exact element found at index", result)
-    else:
-        print("Element not found")
