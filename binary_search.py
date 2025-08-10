cd86bd06290edbde89a34360d99c91b2f0719ef5
Jr Joy <joy2244680@gmail.com>
2025-08-10 22:34:03 +0600

    new update

--- binary_search.py
+++ binary_search.py
@@ -1,24 +1,25 @@
-def binary_search(arr, target):
-    low = 0
-    high = len(arr) - 1
-
-    while low <= high:
-        mid = (low + high) // 2
-
-        if arr[mid] == target:
-            return mid  # Found, return index
-        elif arr[mid] < target:
-            low = mid + 1
-        else:
-            high = mid - 1
-
-    return -1  # Not found
 
 arr = [1, 3, 5, 7, 9, 11, 13, 15]
-target = 3
+target = int(input("Enter number from (1-15) to find : "))
+
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) -1
+
+    while(low <= high):             #its a condition for valid range, if low is greater than high then the range excedded
+        mid = (low + high) // 2     #its the method of divide the data in two parts
+        if(arr[mid] == target):     #if mid equals to target then we got the value
+            return mid              #return the value
+        elif(arr[mid] < target):    #if mid is less than target then 
+            low = mid + 1           #go to the left side for search
+        else:
+            high = mid - 1          #if mid is greater than target then go to the right side for search
+    return -1
+    
 result = binary_search(arr, target)
 
-if result != -1:
-    print(f"Target {target} found at index {result}")
+if(result != -1):
+    print(f"{target} found in index {result}")
 else:
-    print("Target not found")
+    print("Not found")
--- linear_search.py
+++ linear_search.py
@@ -5,16 +5,16 @@
 given_list = [10, 20, 30, 40, 50, 700]
 target = int(input("Enter the number : "))
 
-def linear_search(given_list,target): #create a function with list & targrt
-    for index in range(len(given_list)): #Getting the index of list
-        if(given_list[index]==target): #Checking the list element through index with target element
-            return index  #If its equal then return the index number
-    return -1  #Neither -1
+def linear_search(given_list,target):       #create a function with list & targrt
+    for index in range(len(given_list)):    #Getting the index of list
+        if(given_list[index]==target):      #Checking the list element through index with target element
+            return index                    #If its equal then return the index number
+    return -1                               #Neither -1
 
-result = linear_search(given_list, target) #Storing the function result in a variable
+result = linear_search(given_list, target)  #Storing the function result in a variable
 
-#Condition for printing output
-if(result != -1):  
+
+if(result != -1):                           #Condition for printing output
     print(f"The element is fount at index : {result}")
 else:
     print("The element not found.")
