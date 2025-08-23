ae21b048b17b90da407bde85f80391debfa86749
Jr Joy <joy2244680@gmail.com>
2025-08-23 18:06:50 +0600

    updated

--- linear_search.py
+++ linear_search.py
@@ -3,14 +3,14 @@
 #then output showing by condition.....
 
 given_list = [10, 20, 30, 40, 50, 700]
-target = int(input("Enter the number : "))
+target = 50
 
 joy = len(given_list)
 
 def linear_search(given_list,target):       #create a function with list & targrt
-    for i in range(joy):    #Getting the index of list
-        if(given_list[i]==target):      #Checking the list element through index with target element
-            return i                    #If its equal then return the index number
+    for i in range(joy):                    #Getting the index of list
+        if(given_list[i]==target):          #Checking the list element through index with target element
+            return i                        #If its equal then return the index number
     return -1                               #Neither -1
 
 result = linear_search(given_list, target)  #Storing the function result in a variable
