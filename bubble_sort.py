ebeddae0b3688a699fc46453369e49ef69edcf76
Jr Joy <joy2244680@gmail.com>
2025-08-25 00:53:42 +0600

    print

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,9 +4,9 @@ numbers = list(map(int,user_input.split()))
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):  # number of passes
-
+        print("value of i :",i)
         for j in range(0,n-i-1):  # last i elements already sorted
-
+            print("\n value of j :",j)
             if arr[j] > arr[j + 1]:  # wrong order
 
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]  # swap
